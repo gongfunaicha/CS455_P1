18de129b7d487a5f785fc10bf85aeae09832e6c7
Chen Wang <gongfunaicha@gmail.com>
2017-02-05 17:58:39 -0700

    Updated port binding mechanism.

--- src/cs455/overlay/transport/TCPServerThread.java
+++ src/cs455/overlay/transport/TCPServerThread.java
@@ -11,6 +11,7 @@ public class TCPServerThread extends Thread {
 
     private ServerSocket serverSocket = null;
     private String HostIP = null;
+    private int port = 0;
     private Registry registry;
 
     public TCPServerThread(Registry reg)
@@ -27,23 +28,18 @@ public class TCPServerThread extends Thread {
             System.out.print("Unable to obtain host address.");
             HostIP = "Unknown";
         }
-        boolean success = false;
-        short port = 30000;
-        // Try to bind to port until success
-        while (!success)
-        {
-            try
-            {
-                success = true;
-                serverSocket = new ServerSocket(port, 50);
-            }
-            catch (IOException IOE)
-            {
-                success = false;
-                port += 1;
-            }
+        // Try to bind to an available port
+        try {
+            serverSocket = new ServerSocket(0);
+        } catch (IOException e) {
+            System.out.println("Unable to bind to an available port. Program will now exit.");
+            System.exit(1);
         }
-        System.out.println("Registry is now running on IP: " + HostIP + " Port: " + String.valueOf(port));
+
+        // assign port to the real port number
+        port = serverSocket.getLocalPort();
+
+        System.out.println("Registry is now running on IP: " + HostIP + " Port: " + port);
         //TODO: Accept incoming connections and handle them
     }
 }
