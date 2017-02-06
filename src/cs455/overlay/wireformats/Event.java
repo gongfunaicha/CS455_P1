f0a28b22d6e3433fa16d0dc2200a636ed558546f
Chen Wang <gongfunaicha@gmail.com>
2017-02-06 11:42:03 -0700

    Implemented Register Request Wireformat

--- src/cs455/overlay/wireformats/Event.java
+++ src/cs455/overlay/wireformats/Event.java
@@ -1,7 +1,9 @@
 package cs455.overlay.wireformats;
 
+import java.io.IOException;
+
 // Interface Event, all wire format protocols need to implement Event
 public interface Event {
     Protocol getType();
-    byte[] getBytes();
+    byte[] getBytes() throws IOException;
 }
---
+++ src/cs455/overlay/wireformats/RegisterRequest.java
@@ -0,0 +1,53 @@
+package cs455.overlay.wireformats;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class RegisterRequest implements Event{
+
+    private String IP = null;
+    private int port = 0;
+
+    public RegisterRequest(String IPAddr, int portnum)
+    {
+        this.IP = IPAddr;
+        this.port = portnum;
+    }
+
+    @Override
+    public Protocol getType() {
+        return Protocol.REGISTER_REQUEST;
+    }
+
+    // getBytes is used to get byte[] to send
+    // Format: int Message_Type, int len_IP, char[] IP, int port_num
+    @Override
+    public byte[] getBytes() throws IOException{
+        byte[] marshalledBytes = null;
+        ByteArrayOutputStream baOutputStream = new ByteArrayOutputStream();
+        DataOutputStream dout = new DataOutputStream(new BufferedOutputStream(baOutputStream));
+
+        // Write Message Type
+        dout.writeInt(Protocol.REGISTER_REQUEST.getValue());
+
+        // Write IP Length
+        dout.writeInt(IP.length());
+
+        // Write IP
+        byte[] IPBytes = IP.getBytes();
+        dout.write(IPBytes);
+
+        // Write Port Number
+        dout.writeInt(port);
+
+        dout.flush();
+        marshalledBytes = baOutputStream.toByteArray();
+
+        baOutputStream.close();
+        dout.close();
+
+        return marshalledBytes;
+    }
+}
