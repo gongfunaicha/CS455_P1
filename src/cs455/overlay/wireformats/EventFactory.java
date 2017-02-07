c138ccc8f66fa9f5ead0765ce8d4969e69c6b4cd
Chen Wang <gongfunaicha@gmail.com>
2017-02-06 17:17:53 -0700

    Decoded Register Request packet

--- src/cs455/overlay/wireformats/EventFactory.java
+++ src/cs455/overlay/wireformats/EventFactory.java
@@ -1,5 +1,8 @@
 package cs455.overlay.wireformats;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
 
 // EventFactory is responsible of creating instance of subclass of Event based on the data it received
@@ -7,7 +10,28 @@ public class EventFactory {
 
     public static Event createEventFromData(byte[] data) throws IOException
     {
-        return null;
+        ByteArrayInputStream baInputStream = new ByteArrayInputStream(data);
+        DataInputStream din = new DataInputStream(new BufferedInputStream(baInputStream));
+
+        int numtype = din.readInt();
+        Protocol type = Protocol.valueOf(numtype);
+
+        switch (type)
+        {
+            case REGISTER_REQUEST:
+                // remaining format: int len_IP, char[] IP, int port_num
+                int len_IP = din.readInt();
+                byte[] IPBytes = new byte[len_IP];
+                din.readFully(IPBytes,0,len_IP);
+                String IP = new String(IPBytes);
+                int port = din.readInt();
+                // Create RegisterRequest instance
+                RegisterRequest registerRequest = new RegisterRequest(IP, port);
+                return registerRequest;
+            default:
+                System.out.println("Invalid message type received.");
+                return null;
+        }
     }
 
 }
